2492775bc8e210e07c30c74a4ad4e5b8b0c2f92c
LAUREN MCGUIRE <>
2022-08-17 02:39:59 +0000

    Initial commit

--- GPT3.py
+++ GPT3.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import load_dotenv
+
 import openai
 
-load_dotenv()
-openai.api_key = os.environ.get('OPENAI_KEY')
+my_secret = os.environ['OPENAI_API_KEY']
+
 completion = openai.Completion()
 
 # Default chat setting
@@ -17,15 +17,14 @@ AI: I am an AI created by OpenAI. How can I help you today?
 chat_log = None
 
 
-# Parameters might need to be tweaked for better results
 def ask(question, chat_log=None):
     if chat_log is None:
         chat_log = start_chat_log
     prompt = f'{chat_log}Human: {question}\nAI:'
     response = completion.create(
-        prompt=prompt, engine="davinci", stop=["\n\n", "Human: "], temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-        max_tokens=250)
+        prompt=prompt, engine="davinci", stop=["\n\n", "Human: "], temperature=0.7,
+        top_p=1, frequency_penalty=.3, presence_penalty=0.6, best_of=1,
+        max_tokens=1500)
     answer = response.choices[0].text.strip()
     return answer
 
